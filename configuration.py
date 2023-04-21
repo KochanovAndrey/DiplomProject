51c4e7830ecdd792a738278c9cd5fef9a8c89e10
Андрей <126569103+KochanovAndrey@users.noreply.github.com>
2023-04-21 19:33:33 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://e8a61574-f454-4651-a541-9187cc0c877b.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://e24955fc-a526-4681-ab63-1de2c54a9278.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track?t="
